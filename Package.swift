059d4470b2da49dfce33f807fff5cbb8e06acf32
mr-elbe5 <miro@elbe5.de>
2022-06-09 19:21:43 +0200

    latest packages

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "SwiftyBandikaCL",
     defaultLocalization: "en",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v11)],
     dependencies: [
-        .package(url: "https://github.com/mr-elbe5/BandikaSwiftBase", from: "1.2.1"),
+        .package(url: "https://github.com/mr-elbe5/BandikaSwiftBase", from: "1.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
